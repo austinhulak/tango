6eb9cceed40b9fbc2892c8d6926137d713edb30d
Austin Hulak <austinhulak@gmail.com>
2013-10-27 13:45:50 -0700

    added static, media, and template variables

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 
 def index(request):
-    return HttpResponse('Rango says hello world, click here for the <a href="/rango/about/">about</a> page')
+    context = RequestContext(request)
+
+    context_dict = {'boldmessage': "I am from the context"}
+
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse('This is the about page!!! Go back to the rango <a href="/rango/">home page</a>')
